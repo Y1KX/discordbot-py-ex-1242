eba456c0c634d5d25dc30be1fa4d1a440c20939a
Y1KX <122628991+Y1KX@users.noreply.github.com>
2023-02-28 15:15:56 +0900

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,15 @@
+# 웹훅 적으세여
+
+관리자=[830382976096796704, 954148883875893328] #관리자 id
+서버이름="도박"
+입출금로그=1076190690104590376 # 입출금로그 채널 id
+입출금로그웹훅="https://discord.com/api/webhooks/1076191530064294012/DW2_F41U9n3Kr67ddI9OFKANpuBy-K_ndZRfVjM4Vc6hQlXVDf6xw65s74YEJpjq29Kj" # 입출금로그 채널 웹훅
+계좌번호="토스뱅크 190854291946 이정연" # 은행명 계좌번호 예금주
+충전채널=1076190638116188160
+요청채널=1079996608332177510
+코인채널=1079963764792758373
+컬쳐계정="yeoncute10"
+컬쳐비번="*YeOn0926"
+api토큰=""
+
+봇토큰="MTA3OTk2MTEzMjI5MjM5NTA2OQ.GrP-oE.dpPevB4sV3RAbU65XEEC2jqsbMqXaEcrz0sQxM"
